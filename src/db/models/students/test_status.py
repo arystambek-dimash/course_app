1cabe21fa9ab62b24d3a7142dc002d38b94413de
arystambek-dimash <arystambekdimash005@gmail.com>
2024-09-02 22:14:11 +0500

    :triangular_flag_on_post: removed completed at column

--- src/db/models/students/test_status.py
+++ src/db/models/students/test_status.py
@@ -14,7 +14,6 @@ class UserTestStatusesOrm(Base):
     test_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey('student_topic_tests.id', ondelete="CASCADE"))
     completed: Mapped[bool] = mapped_column(sa.Boolean, default=False)
     max_earned_score: Mapped[float] = mapped_column(sa.Float, nullable=True)
-    completed_at: Mapped[datetime] = mapped_column(sa.DateTime, nullable=True)
 
     user = relationship('UsersOrm', back_populates='test_statuses')
     test = relationship('TestsOrm', back_populates='user_statuses')
