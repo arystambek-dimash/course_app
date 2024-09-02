c7a1f0ed4c83a25c1e5739612e82bf0ca4ad05de
arystambek-dimash <arystambekdimash005@gmail.com>
2024-09-02 22:13:25 +0500

    :triangular_flag_on_post: removed completed at column

--- src/db/models/students/topic_status.py
+++ src/db/models/students/topic_status.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship, Mapped, mapped_column, Session
 
@@ -13,7 +11,6 @@ class UserTopicStatusesOrm(Base):
     user_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey('users.id', ondelete='CASCADE'))
     topic_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey('student_topics.id', ondelete='CASCADE'))
     completed: Mapped[bool] = mapped_column(sa.Boolean, default=False)
-    completed_at: Mapped[datetime] = mapped_column(sa.DateTime, nullable=True)
 
     user = relationship('UsersOrm', back_populates='topic_statuses')
     topic = relationship('TopicsOrm', back_populates='user_statuses')
